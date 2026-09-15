6cd093204d2e0d29aa50c57e3d221de05c1fd123
dinaarshad439 <dinaarshad439@gmail.com>
2026-09-15 21:18:47 +0300

    Question 2 answer

--- C#Basics03/Program.cs
+++ C#Basics03/Program.cs
@@ -7,11 +7,25 @@
 
             #region (Q1) Accessing the second element using index 1
 
-            double[] Prices = { 25.5, 40.0, 33.75 };
-            Console.WriteLine($"The second price : {Prices[1]}");
+            //double[] Prices = { 25.5, 40.0, 33.75 };
+            //Console.WriteLine($"The second price : {Prices[1]}");
 
 
             #endregion
+
+            #region (Q2) Accessing an element in a 2D array
+
+            int[,] ShelfCopies =
+            {
+               { 3, 5 },
+               { 1, 4 }
+            };
+
+            Console.WriteLine($"Number of copies : {ShelfCopies[1, 0]}");
+
+
+            #endregion
+
 
 
 
