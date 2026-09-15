b4413775ef4d9611e559de9bba5d95a31089c060
dinaarshad439 <dinaarshad439@gmail.com>
2026-09-15 21:10:45 +0300

    Question 1 answer

--- C#Basics03/Program.cs
+++ C#Basics03/Program.cs
@@ -4,14 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
+
+            #region (Q1) Accessing the second element using index 1
+
+            double[] Prices = { 25.5, 40.0, 33.75 };
+            Console.WriteLine($"The second price : {Prices[1]}");
 
 
-            
+            #endregion
 
-           
+
+
+
         }
 
-        
+
     }
 }
